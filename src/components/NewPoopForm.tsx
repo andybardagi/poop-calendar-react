f31f74c60b96769a56ca7f5c3b2bd7bf203a28ae
andybardagi <andresbardagi2002@gmail.com>
2022-02-09 15:39:43 -0300

    Fix n.map is not a function

--- src/components/NewPoopForm.tsx
+++ src/components/NewPoopForm.tsx
@@ -7,7 +7,52 @@ import NewPoop from '../pages/NewPoop';
 import { newPoop } from '../services/reviews.service';
 
 export function NewPoopForm() {
-    const [users, setUsers] = useState([] as User[]);
+    const EXAMPLE_USERS = [
+        {
+            id: '6203296173e305527b35e2fd',
+            username: 'ginomassei',
+            firstname: 'Gino',
+            lastname: 'Massei',
+        },
+        {
+            id: '62032d79087d4e896eafd0b4',
+            username: 'abardagi',
+            firstname: 'Andy',
+            lastname: 'Bardagi',
+        },
+        {
+            id: '6203b6072141bd3863d64f69',
+            username: 'lorensala',
+            firstname: 'Lorenzo',
+            lastname: 'Sala',
+        },
+        {
+            id: '6203b6152141bd3863d64f6a',
+            username: 'ramisomavilla',
+            firstname: 'Ramiro',
+            lastname: 'Somavilla',
+        },
+        {
+            id: '6203b6292141bd3863d64f6b',
+            username: 'juan_tamosaitis',
+            firstname: 'Juan',
+            lastname: 'Tamosaitis',
+        },
+        {
+            id: '6203b6372141bd3863d64f6c',
+            username: 'lucaslopez',
+            firstname: 'Lucas',
+            lastname: 'Lopez',
+        },
+        {
+            id: '6203f1619f864a3f6ef789dd',
+            username: 'usuariodeprueba',
+            firstname: 'Usuario',
+            lastname: 'Prueba',
+        },
+    ];
+
+    const [users, setUsers] = useState(EXAMPLE_USERS);
     const [input, setInput] = useState({
         userId: '',
     });
@@ -25,50 +70,6 @@ export function NewPoopForm() {
                     duration: 5000,
                     isClosable: true,
                 });
-                setUsers([
-                    {
-                        id: '6203296173e305527b35e2fd',
-                        username: 'ginomassei',
-                        firstname: 'Gino',
-                        lastname: 'Massei',
-                    },
-                    {
-                        id: '62032d79087d4e896eafd0b4',
-                        username: 'abardagi',
-                        firstname: 'Andy',
-                        lastname: 'Bardagi',
-                    },
-                    {
-                        id: '6203b6072141bd3863d64f69',
-                        username: 'lorensala',
-                        firstname: 'Lorenzo',
-                        lastname: 'Sala',
-                    },
-                    {
-                        id: '6203b6152141bd3863d64f6a',
-                        username: 'ramisomavilla',
-                        firstname: 'Ramiro',
-                        lastname: 'Somavilla',
-                    },
-                    {
-                        id: '6203b6292141bd3863d64f6b',
-                        username: 'juan_tamosaitis',
-                        firstname: 'Juan',
-                        lastname: 'Tamosaitis',
-                    },
-                    {
-                        id: '6203b6372141bd3863d64f6c',
-                        username: 'lucaslopez',
-                        firstname: 'Lucas',
-                        lastname: 'Lopez',
-                    },
-                    {
-                        id: '6203f1619f864a3f6ef789dd',
-                        username: 'usuariodeprueba',
-                        firstname: 'Usuario',
-                        lastname: 'Prueba',
-                    },
-                ]);
             });
     }, []);
 
@@ -100,15 +101,17 @@ export function NewPoopForm() {
             );
     };
 
+    const UserOptions = EXAMPLE_USERS.map((user) => (
+        <option key={user.id} value={user.id}>
+            {user.username}
+        </option>
+    ));
+
     return (
         <>
             <Flex direction="row">
                 <Select onChange={changeSelected}>
-                    {users.map((user) => (
-                        <option key={user.id} value={user.id}>
-                            {user.username}
-                        </option>
-                    ))}
+                    { users.length != 0 ? UserOptions : <></>}
                 </Select>
                 <Button ms={3} onClick={submitPoop}>
                     Register Poop
